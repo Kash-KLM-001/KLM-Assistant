368eeccfd2585d6e4f7adcced40e251a432ef2b5
Kash <kashlabsmc001@gmail.com>
2026-08-18 12:10:39 +0000

    Added better terminal

--- kcli.py
+++ kcli.py
@@ -1,24 +1,33 @@
 import subprocess
-def K_CLI():
+import datetime
+def K_CLI(name):
+    time = datetime.datetime.now()
+    
     running = True
     while running:
-        input_cli = input("[K-CLI]> ")
+        pwd = subprocess.check_output("pwd",text=True)
+        input_cli = input(f'''╭─[User-{name}]─[{time:%Hh-%Mm-%Ss}]
+├─[{pwd}]
+╰─❯''')
         if " " in input_cli:
             try:
                 input_cli = input_cli.split(" ")
                 subprocess.run(input_cli)
-                print("\n")
+                
             except Exception as e:
                 print("\n",e,"\n")
                 continue
         elif input_cli == "exit":
             running = False
+            break
         
         else:
             try:
                 subprocess.run([input_cli])
-                print("\n")
+                
             except Exception as e:
                 print("\n",e,"\n")
                 continue
 
+if __name__ == "__main__":
+    K_CLI("kk")
